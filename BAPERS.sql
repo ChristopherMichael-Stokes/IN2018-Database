15cff63c3d073f7a6e9ff5e75013e22e61a5c302
ChristopherMichael-Stokes <christophermichaelstokes@gmail.com>
2018-02-26 01:09:30 +0000

    fixed payment entity, added description to job entity, and added sample data

--- BAPERS.sql
+++ BAPERS.sql
@@ -49,31 +49,6 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `BAPERS`.`Payment_Info`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `BAPERS`.`Payment_Info` ;
-
-CREATE TABLE IF NOT EXISTS `BAPERS`.`Payment_Info` (
-  `payment_id` INT NOT NULL,
-  `amount_paid` INT NOT NULL,
-  `payment_type` VARCHAR(10) NOT NULL,
-  `date_paid` DATE NOT NULL,
-  PRIMARY KEY (`payment_id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `BAPERS`.`Discount_Plan`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `BAPERS`.`Discount_Plan` ;
-
-CREATE TABLE IF NOT EXISTS `BAPERS`.`Discount_Plan` (
-  `type` VARCHAR(10) NOT NULL,
-  PRIMARY KEY (`type`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `BAPERS`.`Customer_Account`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `BAPERS`.`Customer_Account` ;
@@ -87,14 +62,7 @@ CREATE TABLE IF NOT EXISTS `BAPERS`.`Customer_Account` (
   `surname` VARCHAR(45) NOT NULL,
   `house_phone` VARCHAR(11) NOT NULL,
   `mobile_phone` VARCHAR(11) NULL,
-  `fk_type` VARCHAR(10) NOT NULL,
-  PRIMARY KEY (`account_number`),
-  INDEX `fk_Customer_Account_Discount_Plan1_idx` (`fk_type` ASC),
-  CONSTRAINT `fk_Customer_Account_Discount_Plan1`
-    FOREIGN KEY (`fk_type`)
-    REFERENCES `BAPERS`.`Discount_Plan` (`type`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`account_number`))
 ENGINE = InnoDB;
 
 
@@ -104,27 +72,33 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `BAPERS`.`Job` ;
 
 CREATE TABLE IF NOT EXISTS `BAPERS`.`Job` (
-  `job_id` INT NOT NULL,
+  `job_id` VARCHAR(6) NOT NULL,
+  `description` VARCHAR(45) NOT NULL,
   `amount_due` INT NOT NULL,
   `deadline` DATETIME NULL,
-  `start_time` DATETIME NOT NULL,
-  `end_time` DATETIME NULL,
   `urgent` TINYINT(1) NOT NULL,
-  `fk_payment_id` INT NOT NULL,
   `fk_account_number` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`job_id`, `fk_payment_id`, `fk_account_number`),
-  INDEX `fk_Job_Payment_Info1_idx` (`fk_payment_id` ASC),
+  PRIMARY KEY (`job_id`),
   INDEX `fk_Job_Customer_Account1_idx` (`fk_account_number` ASC),
-  CONSTRAINT `fk_Job_Payment_Info1`
-    FOREIGN KEY (`fk_payment_id`)
-    REFERENCES `BAPERS`.`Payment_Info` (`payment_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `fk_Job_Customer_Account1`
     FOREIGN KEY (`fk_account_number`)
     REFERENCES `BAPERS`.`Customer_Account` (`account_number`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `BAPERS`.`Payment_Info`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `BAPERS`.`Payment_Info` ;
+
+CREATE TABLE IF NOT EXISTS `BAPERS`.`Payment_Info` (
+  `payment_id` INT NOT NULL,
+  `amount_paid` INT NOT NULL,
+  `payment_type` VARCHAR(10) NOT NULL,
+  `date_paid` DATE NOT NULL,
+  PRIMARY KEY (`payment_id`))
 ENGINE = InnoDB;
 
 
@@ -187,28 +161,31 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `BAPERS`.`Tasks`
+-- Table `BAPERS`.`Discount_Plan`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `BAPERS`.`Tasks` ;
+DROP TABLE IF EXISTS `BAPERS`.`Discount_Plan` ;
 
-CREATE TABLE IF NOT EXISTS `BAPERS`.`Tasks` (
-  `fk_job_id` INT NOT NULL,
-  `fk_payment_id` INT NOT NULL,
-  `fk_account_number` VARCHAR(45) NOT NULL,
+CREATE TABLE IF NOT EXISTS `BAPERS`.`Discount_Plan` (
+  `type` VARCHAR(10) NOT NULL,
+  PRIMARY KEY (`type`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `BAPERS`.`Job_Task`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `BAPERS`.`Job_Task` ;
+
+CREATE TABLE IF NOT EXISTS `BAPERS`.`Job_Task` (
+  `fk_job_id` VARCHAR(6) NOT NULL,
   `fk_task_id` INT NOT NULL,
   `start_time` DATETIME NOT NULL,
   `end_time` DATETIME NULL,
-  `discount` INT NOT NULL,
   `fk_staff_id` VARCHAR(15) NOT NULL,
-  PRIMARY KEY (`fk_job_id`, `fk_payment_id`, `fk_account_number`, `fk_task_id`),
+  PRIMARY KEY (`fk_job_id`, `fk_task_id`),
   INDEX `fk_Job_has_Task_Task1_idx` (`fk_task_id` ASC),
-  INDEX `fk_Job_has_Task_Job1_idx` (`fk_job_id` ASC, `fk_payment_id` ASC, `fk_account_number` ASC),
   INDEX `fk_Tasks_Staff1_idx` (`fk_staff_id` ASC),
-  CONSTRAINT `fk_Job_has_Task_Job1`
-    FOREIGN KEY (`fk_job_id` , `fk_payment_id` , `fk_account_number`)
-    REFERENCES `BAPERS`.`Job` (`job_id` , `fk_payment_id` , `fk_account_number`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+  INDEX `fk_Job_Task_Job1_idx` (`fk_job_id` ASC),
   CONSTRAINT `fk_Job_has_Task_Task1`
     FOREIGN KEY (`fk_task_id`)
     REFERENCES `BAPERS`.`Task` (`task_id`)
@@ -217,6 +194,101 @@ CREATE TABLE IF NOT EXISTS `BAPERS`.`Tasks` (
   CONSTRAINT `fk_Tasks_Staff1`
     FOREIGN KEY (`fk_staff_id`)
     REFERENCES `BAPERS`.`Staff` (`staff_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Job_Task_Job1`
+    FOREIGN KEY (`fk_job_id`)
+    REFERENCES `BAPERS`.`Job` (`job_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `BAPERS`.`Discount`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `BAPERS`.`Discount` ;
+
+CREATE TABLE IF NOT EXISTS `BAPERS`.`Discount` (
+  `fk_type` VARCHAR(10) NOT NULL,
+  `fk_account_number` VARCHAR(45) NOT NULL,
+  INDEX `fk_Discount_Discount_Plan1_idx` (`fk_type` ASC),
+  PRIMARY KEY (`fk_account_number`),
+  CONSTRAINT `fk_Discount_Discount_Plan1`
+    FOREIGN KEY (`fk_type`)
+    REFERENCES `BAPERS`.`Discount_Plan` (`type`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Discount_Customer_Account1`
+    FOREIGN KEY (`fk_account_number`)
+    REFERENCES `BAPERS`.`Customer_Account` (`account_number`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `BAPERS`.`Task_Discount`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `BAPERS`.`Task_Discount` ;
+
+CREATE TABLE IF NOT EXISTS `BAPERS`.`Task_Discount` (
+  `fk_account_number` VARCHAR(45) NOT NULL,
+  `fk_task_id` INT NOT NULL,
+  `percentage` FLOAT NOT NULL,
+  PRIMARY KEY (`fk_account_number`, `fk_task_id`),
+  INDEX `fk_Customer_Account_has_Task_Task1_idx` (`fk_task_id` ASC),
+  INDEX `fk_Discounts_Discount1_idx` (`fk_account_number` ASC),
+  CONSTRAINT `fk_Customer_Account_has_Task_Task1`
+    FOREIGN KEY (`fk_task_id`)
+    REFERENCES `BAPERS`.`Task` (`task_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Discounts_Discount1`
+    FOREIGN KEY (`fk_account_number`)
+    REFERENCES `BAPERS`.`Discount` (`fk_account_number`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `BAPERS`.`Discount_Band`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `BAPERS`.`Discount_Band` ;
+
+CREATE TABLE IF NOT EXISTS `BAPERS`.`Discount_Band` (
+  `fk_account_number` VARCHAR(45) NOT NULL,
+  `price` INT NOT NULL,
+  `percentage` FLOAT NOT NULL,
+  PRIMARY KEY (`fk_account_number`, `price`),
+  CONSTRAINT `fk_table1_Discount1`
+    FOREIGN KEY (`fk_account_number`)
+    REFERENCES `BAPERS`.`Discount` (`fk_account_number`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `BAPERS`.`Payment`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `BAPERS`.`Payment` ;
+
+CREATE TABLE IF NOT EXISTS `BAPERS`.`Payment` (
+  `fk_payment_id` INT NOT NULL,
+  `fk_job_id` VARCHAR(6) NOT NULL,
+  PRIMARY KEY (`fk_payment_id`, `fk_job_id`),
+  INDEX `fk_Payment_Info_has_Job_Job1_idx` (`fk_job_id` ASC),
+  INDEX `fk_Payment_Info_has_Job_Payment_Info1_idx` (`fk_payment_id` ASC),
+  CONSTRAINT `fk_Payment_Info_has_Job_Payment_Info1`
+    FOREIGN KEY (`fk_payment_id`)
+    REFERENCES `BAPERS`.`Payment_Info` (`payment_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Payment_Info_has_Job_Job1`
+    FOREIGN KEY (`fk_job_id`)
+    REFERENCES `BAPERS`.`Job` (`job_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -240,14 +312,17 @@ COMMIT;
 
 
 -- -----------------------------------------------------
--- Data for table `BAPERS`.`Discount_Plan`
+-- Data for table `BAPERS`.`Staff`
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `BAPERS`;
-INSERT INTO `BAPERS`.`Discount_Plan` (`type`) VALUES ('fixed');
-INSERT INTO `BAPERS`.`Discount_Plan` (`type`) VALUES ('variable');
-INSERT INTO `BAPERS`.`Discount_Plan` (`type`) VALUES ('flexible');
-INSERT INTO `BAPERS`.`Discount_Plan` (`type`) VALUES ('none');
+INSERT INTO `BAPERS`.`Staff` (`staff_id`, `first_name`, `surname`, `passphrase`, `fk_type`) VALUES ('0000', 'chris', 'stokes', 'b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86', 'office manager');
+INSERT INTO `BAPERS`.`Staff` (`staff_id`, `first_name`, `surname`, `passphrase`, `fk_type`) VALUES ('0001', 'john', 'nash', 'b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86', 'technician');
+INSERT INTO `BAPERS`.`Staff` (`staff_id`, `first_name`, `surname`, `passphrase`, `fk_type`) VALUES ('0002', 'lee', 'hong', 'b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86', 'technician');
+INSERT INTO `BAPERS`.`Staff` (`staff_id`, `first_name`, `surname`, `passphrase`, `fk_type`) VALUES ('0003', 'julie', 'abbot', 'b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86', 'technician');
+INSERT INTO `BAPERS`.`Staff` (`staff_id`, `first_name`, `surname`, `passphrase`, `fk_type`) VALUES ('0004', 'marina', 'scott', 'b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86', 'technician');
+INSERT INTO `BAPERS`.`Staff` (`staff_id`, `first_name`, `surname`, `passphrase`, `fk_type`) VALUES ('0005', 'stewart', 'pask', 'b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86', 'technician');
+INSERT INTO `BAPERS`.`Staff` (`staff_id`, `first_name`, `surname`, `passphrase`, `fk_type`) VALUES ('0006', 'rich', 'evans', 'b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86', 'office manager');
 
 COMMIT;
 
@@ -257,9 +332,20 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `BAPERS`;
-INSERT INTO `BAPERS`.`Customer_Account` (`account_number`, `email`, `account_holder_name`, `title`, `first_name`, `surname`, `house_phone`, `mobile_phone`, `fk_type`) VALUES ('acc0001', NULL, 'City University', 'prof', 'david', 'rhind', '02070408000', NULL, 'fixed');
-INSERT INTO `BAPERS`.`Customer_Account` (`account_number`, `email`, `account_holder_name`, `title`, `first_name`, `surname`, `house_phone`, `mobile_phone`, `fk_type`) VALUES ('acc0002', NULL, 'AirVia Ltd', 'mr', 'boris', 'berezovsky', '02073218523', NULL, 'flexible');
-INSERT INTO `BAPERS`.`Customer_Account` (`account_number`, `email`, `account_holder_name`, `title`, `first_name`, `surname`, `house_phone`, `mobile_phone`, `fk_type`) VALUES ('acc0003', NULL, 'InfoPharma Ltd', 'mr', 'alex', 'wright', '02073218001', NULL, 'variable');
+INSERT INTO `BAPERS`.`Customer_Account` (`account_number`, `email`, `account_holder_name`, `title`, `first_name`, `surname`, `house_phone`, `mobile_phone`) VALUES ('acc0001', NULL, 'City University', 'prof', 'david', 'rhind', '02070408000', NULL);
+INSERT INTO `BAPERS`.`Customer_Account` (`account_number`, `email`, `account_holder_name`, `title`, `first_name`, `surname`, `house_phone`, `mobile_phone`) VALUES ('acc0002', NULL, 'AirVia Ltd', 'mr', 'boris', 'berezovsky', '02073218523', NULL);
+INSERT INTO `BAPERS`.`Customer_Account` (`account_number`, `email`, `account_holder_name`, `title`, `first_name`, `surname`, `house_phone`, `mobile_phone`) VALUES ('acc0003', NULL, 'InfoPharma Ltd', 'mr', 'alex', 'wright', '02073218001', NULL);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `BAPERS`.`Job`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `BAPERS`;
+INSERT INTO `BAPERS`.`Job` (`job_id`, `description`, `amount_due`, `deadline`, `urgent`, `fk_account_number`) VALUES ('abn54', '5 x 4 B& W copy negatives', 1900, NULL, false, 'acc0001');
+INSERT INTO `BAPERS`.`Job` (`job_id`, `description`, `amount_due`, `deadline`, `urgent`, `fk_account_number`) VALUES ('acn54', '5 x 4 Colour copy negatives ', 1900, NULL, false, 'acc0001');
 
 COMMIT;
 
@@ -269,7 +355,9 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `BAPERS`;
-INSERT INTO `BAPERS`.`Address` (`address_line1`, `address_line2`, `postcode`, `city`, `country`, `fk_account_number`) VALUES ('Northhampton Square', NULL, 'ec1v 0hb', 'london', 'england', 'acc0001');
+INSERT INTO `BAPERS`.`Address` (`address_line1`, `address_line2`, `postcode`, `city`, `country`, `fk_account_number`) VALUES ('northhampton square', NULL, 'ec1v 0hb', 'london', 'england', 'acc0001');
+INSERT INTO `BAPERS`.`Address` (`address_line1`, `address_line2`, `postcode`, `city`, `country`, `fk_account_number`) VALUES ('12 bond street', NULL, 'wc1v 8hu', 'london', 'england', 'acc0002');
+INSERT INTO `BAPERS`.`Address` (`address_line1`, `address_line2`, `postcode`, `city`, `country`, `fk_account_number`) VALUES ('25 bond street', '', 'wc1v 8ls', 'london', 'england', 'acc0003');
 
 COMMIT;
 
@@ -286,6 +374,73 @@ INSERT INTO `BAPERS`.`Task` (`task_id`, `description`, `location`, `shelf_slot`,
 INSERT INTO `BAPERS`.`Task` (`task_id`, `description`, `location`, `shelf_slot`, `price`, `duration`) VALUES (5, 'colour transparency processing', 'development area', 'dr100', 11030, '03:00:00');
 INSERT INTO `BAPERS`.`Task` (`task_id`, `description`, `location`, `shelf_slot`, `price`, `duration`) VALUES (6, 'use of small copy camera', 'copy room', 'cr16', 830, '01:15:00');
 INSERT INTO `BAPERS`.`Task` (`task_id`, `description`, `location`, `shelf_slot`, `price`, `duration`) VALUES (7, 'mount transparencies', 'finishing room', 'fr5', 5550, '45:00');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `BAPERS`.`Discount_Plan`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `BAPERS`;
+INSERT INTO `BAPERS`.`Discount_Plan` (`type`) VALUES ('fixed');
+INSERT INTO `BAPERS`.`Discount_Plan` (`type`) VALUES ('variable');
+INSERT INTO `BAPERS`.`Discount_Plan` (`type`) VALUES ('flexible');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `BAPERS`.`Job_Task`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `BAPERS`;
+INSERT INTO `BAPERS`.`Job_Task` (`fk_job_id`, `fk_task_id`, `start_time`, `end_time`, `fk_staff_id`) VALUES ('abn54', 1, '2018-01-13 12:00:00', '2018-01-13 12:20:00', '0001');
+INSERT INTO `BAPERS`.`Job_Task` (`fk_job_id`, `fk_task_id`, `start_time`, `end_time`, `fk_staff_id`) VALUES ('abn54', 2, '2018-01-13 12:30:00', '2018-01-13 13:10:00', '0002');
+INSERT INTO `BAPERS`.`Job_Task` (`fk_job_id`, `fk_task_id`, `start_time`, `end_time`, `fk_staff_id`) VALUES ('abn54', 3, '2018-01-13 13:20:00', '2018-01-13 13:30:00', '0004');
+INSERT INTO `BAPERS`.`Job_Task` (`fk_job_id`, `fk_task_id`, `start_time`, `end_time`, `fk_staff_id`) VALUES ('acn54', 1, '2018-01-13 12:20:00', '2018-01-13 12:55:00', '0001');
+INSERT INTO `BAPERS`.`Job_Task` (`fk_job_id`, `fk_task_id`, `start_time`, `end_time`, `fk_staff_id`) VALUES ('acn54', 4, '2018-01-13 13:10:00', '2018-01-13 13:50:00', '0002');
+INSERT INTO `BAPERS`.`Job_Task` (`fk_job_id`, `fk_task_id`, `start_time`, `end_time`, `fk_staff_id`) VALUES ('acn54', 3, '2018-01-13 14:00:00', '2018-01-13 14:10:00', '0003');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `BAPERS`.`Discount`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `BAPERS`;
+INSERT INTO `BAPERS`.`Discount` (`fk_type`, `fk_account_number`) VALUES ('fixed', 'acc0001');
+INSERT INTO `BAPERS`.`Discount` (`fk_type`, `fk_account_number`) VALUES ('flexible', 'acc0002');
+INSERT INTO `BAPERS`.`Discount` (`fk_type`, `fk_account_number`) VALUES ('variable', 'acc0003');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `BAPERS`.`Task_Discount`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `BAPERS`;
+INSERT INTO `BAPERS`.`Task_Discount` (`fk_account_number`, `fk_task_id`, `percentage`) VALUES ('acc0002', 1, 0.01);
+INSERT INTO `BAPERS`.`Task_Discount` (`fk_account_number`, `fk_task_id`, `percentage`) VALUES ('acc0002', 2, 0.01);
+INSERT INTO `BAPERS`.`Task_Discount` (`fk_account_number`, `fk_task_id`, `percentage`) VALUES ('acc0002', 3, 0);
+INSERT INTO `BAPERS`.`Task_Discount` (`fk_account_number`, `fk_task_id`, `percentage`) VALUES ('acc0002', 4, 0.02);
+INSERT INTO `BAPERS`.`Task_Discount` (`fk_account_number`, `fk_task_id`, `percentage`) VALUES ('acc0002', 5, 0);
+INSERT INTO `BAPERS`.`Task_Discount` (`fk_account_number`, `fk_task_id`, `percentage`) VALUES ('acc0002', 6, 0.02);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `BAPERS`.`Discount_Band`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `BAPERS`;
+INSERT INTO `BAPERS`.`Discount_Band` (`fk_account_number`, `price`, `percentage`) VALUES ('acc0003', 0, 0);
+INSERT INTO `BAPERS`.`Discount_Band` (`fk_account_number`, `price`, `percentage`) VALUES ('acc0003', 100000, 0.01);
+INSERT INTO `BAPERS`.`Discount_Band` (`fk_account_number`, `price`, `percentage`) VALUES ('acc0003', 200000, 0.02);
+INSERT INTO `BAPERS`.`Discount_Band` (`fk_account_number`, `price`, `percentage`) VALUES ('acc0001', 0, 0.03);
 
 COMMIT;
 
